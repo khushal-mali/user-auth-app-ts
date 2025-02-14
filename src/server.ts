4b02cf79d31b2b1b400e6c16ee6cefad5ef68dd0
Khushal Mali <khushalmali8448@gmail.com>
2025-02-14 17:21:39 +0530

    morgan added

--- src/server.ts
+++ src/server.ts
@@ -5,16 +5,20 @@ import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
 import cors from "cors";
 import bodyParser from "body-parser";
+import morgan from "morgan";
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+
 app.use(cors());
 app.use(bodyParser.json());
+app.use(morgan("dev")); // Logs HTTP requests
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
